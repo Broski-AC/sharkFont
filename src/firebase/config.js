797c251a5adb6274fa2ad0c7b345eddf33c418d7
Broski-AC <acbroski@oakland.edu>
2020-12-05 01:44:10 -0500

    Latest working set

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FRBS_API_KEY,
+    authDomain: process.env.REACT_APP_FRBS_AUTH_DOM,
+    projectId: process.env.REACT_APP_FRBS_PROJ_ID,
+    storageBucket: process.env.REACT_APP_FRBS_STOR_BUCK,
+    messagingSenderId: process.env.REACT_APP_FRBS_MESS_ID,
+    appId: process.env.REACT_APP_FRBS_APP_ID,
+    measurementId: process.env.REACT_APP_FRBS_MEAS_ID
+};
+
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
