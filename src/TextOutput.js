f5808e807cd168cf8f85878f7e35028a1fb67e71
Broski-AC <acbroski@oakland.edu>
2020-12-06 03:18:37 -0500

    Adds text functionality and some additional styling features

--- src/TextOutput.js
+++ src/TextOutput.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './textOutput.css';
 
 function TextOutput(props){
 
+    const [blueColor, setColor] = useState(false)
+
+    useEffect(() => {
+        if (props.translationText !== ''){
+            setColor(true);
+        } else {
+            console.log('No text yet');
+        }
+    }, [props.translationText]);
+
     return (
-        <div className="textOutput"> 
+        <div className={blueColor ? "textOutput" : 'noText'}> 
             <h1 className="sharkFont">{props.translationText}</h1>
         </div>
     )
